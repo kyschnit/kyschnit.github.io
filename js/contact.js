e06d5aad56e40d9a77006f1edd8e536ad3fa96cf
kyschnit <kyschnit@umich.edu>
2021-07-19 22:11:34 -0400

    Changed alerts to modals

--- js/contact.js
+++ js/contact.js
@@ -2,26 +2,6 @@
 	emailjs.init("user_S9naJDiA4QnWLZ8GpMkXx");
 })();
 
-function successAlert() {
-	if($('#alert-failure').hasClass('show')) {
-		$('alert-failure').hide();
-	}
-	$('#alert-success').fadeIn(1000);
-	setTimeout(function() { 
-		$('#alert-success').fadeOut(1000); 
-	}, 5000);
-}
-
-function failureAlert() {
-	if($('#alert-success').hasClass('show')) {
-		$('alert-success').hide();
-	}
-	$('#alert-failure').fadeIn(1000);
-	setTimeout(function() { 
-		$('#alert-failure').fadeOut(1000); 
-	}, 5000);
-}
-
 window.onload = function() {
 	document.getElementById('contact-form').addEventListener('submit', function(event) {
 		event.preventDefault();
@@ -32,11 +12,11 @@ window.onload = function() {
 		.then(function() {
 			console.log('SUCCESS!');
 			document.getElementById('contact-form').reset();
-			successAlert();
+			$('#modal-success').modal("show");
 			
 		}, function(error) {
 			console.log('FAILED...', error);
-			failureAlert();
+			$('#modal-failure').modal("show");
 		});
 	});
 }
