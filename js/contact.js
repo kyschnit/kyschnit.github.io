1e07cc9db418604fdd91da60fd539f025d18fadb
kyschnit <kyschnit@umich.edu>
2021-08-01 11:36:39 -0400

    Lots of formatting changes

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,25 @@
-(function() {
+(function () {
 	emailjs.init("user_S9naJDiA4QnWLZ8GpMkXx");
 })();
 
-window.onload = function() {
-	document.getElementById('contact-form').addEventListener('submit', function(event) {
-		event.preventDefault();
-		// generate a five digit number for the contact_number variable
-		this.contact_number.value = Math.random() * 100000 | 0;
-		// these IDs from the previous steps
-		emailjs.sendForm('contact_service', 'contact_form', this)
-		.then(function() {
-			console.log('SUCCESS!');
-			document.getElementById('contact-form').reset();
-			$('#modal-success').modal("show");
-			
-		}, function(error) {
-			console.log('FAILED...', error);
-			$('#modal-failure').modal("show");
+window.onload = function () {
+	document
+		.getElementById("contact-form")
+		.addEventListener("submit", function (event) {
+			event.preventDefault();
+			// generate a five digit number for the contact_number variable
+			this.contact_number.value = (Math.random() * 100000) | 0;
+			// these IDs from the previous steps
+			emailjs.sendForm("contact_service", "contact_form", this).then(
+				function () {
+					console.log("SUCCESS!");
+					document.getElementById("contact-form").reset();
+					$("#modal-success").modal("show");
+				},
+				function (error) {
+					console.log("FAILED...", error);
+					$("#modal-failure").modal("show");
+				}
+			);
 		});
-	});
-}
+};
