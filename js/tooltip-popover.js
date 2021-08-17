11e12879e56b6f2451e00554f4d1535c589fa2fa
kyschnit <kyschnit@umich.edu>
2021-08-17 01:56:28 -0400

    Color changes, content added.

---
+++ js/tooltip-popover.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function () {
+	$('[data-toggle="popover"]').popover();
+});
+
+$('.popover-dismiss').popover({
+	trigger: 'focus'
+})
