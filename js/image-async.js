0f37ce585de93576ba413b3edfcde533346bb0ba
kyschnit <kyschnit@umich.edu>
2021-12-22 16:11:21 -0500

    Updated resume

---
+++ js/image-async.js
@@ -0,0 +1,16 @@
+var image = document.images[0];
+
+var imgs = [
+	"imgs/user.jpg",
+	"imgs/michigan_logo.png",
+	"imgs/saline_logo.png",
+	"../imgs/background.jpg"
+]
+for (var i = 0; i < document.images.length; i++) {
+	console.log(document.images[i]);
+	var downloadingImage = new Image();
+	downloadingImage.onload = function () {
+		image.src = this.src;
+	};
+	downloadingImage.src = imgs[i];
+}
