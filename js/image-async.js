4816d73dbe8bc59ec5c719230f16bbff3fd21e81
kyschnit <kyschnit@umich.edu>
2022-06-18 21:45:02 -0400

    working on new index

--- js/image-async.js
+++ js/image-async.js
@@ -1,9 +1,9 @@
 var image = document.images[0];
 
 var imgs = [
-	"imgs/user.jpg",
-	"imgs/michigan_logo.png",
-	"imgs/saline_logo.png",
+	"../imgs/user.jpg",
+	"../imgs/michigan_logo.png",
+	"../imgs/saline_logo.png",
 	"../imgs/background.jpg"
 ]
 for (var i = 0; i < document.images.length; i++) {
